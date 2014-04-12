992be889da6b78574b3b55c1ab780dbcc220fa54
YuHua, Wu <je18573@gmail.com>
2014-04-12 18:50:11 +0800

    update MMscripter2.html - delete two box block - change views

--- js/practice.js
+++ js/practice.js
@@ -1,11 +1,5 @@
 $(document).ready(function(){
    $(window).bind('scroll', function() {
-       var navHeight = $( window ).height() - 400;
-             if ($(window).scrollTop() > navHeight ) {
-                 $('nav').addClass('menu_fixed');
-             }
-             else {
-                 $('nav').removeClass('menu_fixed');
-             }
+       $('nav').addClass('menu_fixed');
     });    
 });
